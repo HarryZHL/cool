f2a6ec48b7e5acca982a522c4d8f695f3c9b6254
zhanghelong <zhanghelong@zhongan.io>
2019-11-19 18:58:47 +0800

    update

--- script/rank.js
+++ script/rank.js
@@ -22,12 +22,10 @@ function askCreate () {
 
 function pop (arr) {
   const array = arr
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j]) {
-        const temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]
       }
     }
   }
